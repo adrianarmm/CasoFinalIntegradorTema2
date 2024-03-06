34d79077542ec2d9855b888b43bb3417acb55afc
adrianarmm <areyemor@myuax.com>
2024-03-06 11:14:18 +0100

    creacion static void main

--- src/main/java/ANIMALES/MAINANIMALES.java
+++ src/main/java/ANIMALES/MAINANIMALES.java
@@ -1,4 +1,11 @@
 package ANIMALES;
 
 public class MAINANIMALES {
-}
+
+    public static void main(String[] args) {
+        // Crear instancia de un animal acuático
+        acuatico delfin = new acuatico("Delfín", "Mamífero", "Delfín común", "Gris", "Macho", "10 años",
+                "200 kg", "2 m", "Acuático", "Océano", "Peces y calamares", "Vivíparo", "Nadar en aguas profundas",
+                "Fiebre", "Vacuna contra la fiebre", 1, true, true, true, true, true, "Salada");
+
+    }
