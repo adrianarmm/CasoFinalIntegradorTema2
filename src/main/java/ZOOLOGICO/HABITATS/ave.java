dc2b4445ab148cea939b050ccaefe83badd62229
adrianarmm <areyemor@myuax.com>
2024-03-06 11:36:36 +0100

    creacion ave

--- src/main/java/ZOOLOGICO/HABITATS/ave.java
+++ src/main/java/ZOOLOGICO/HABITATS/ave.java
@@ -1,4 +1,39 @@
 package ZOOLOGICO.HABITATS;
 
-public class ave {
+public class ave extends habitats{
+    boolean puede_volar;
+
+    public ave(float temperatura,float humedad,boolean limpieza, boolean puede_volar) {
+        super(temperatura, humedad, limpieza);
+        this.puede_volar = puede_volar;
+    }
+
+    public ave(float temperatura, float humedad, boolean limpieza) {
+        super(temperatura, humedad, limpieza);
+    }
+
+    public String toString() {
+        return "aviario []";
+    }
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public boolean isPuede_volar() {
+        return puede_volar;
+    }
+
+    public void setPuede_volar(boolean puede_volar) {
+        this.puede_volar = puede_volar;
+    }
+
+
+}
 }
