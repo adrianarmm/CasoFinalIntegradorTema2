2dcf18cfcc920b5ab501252e4e7e37f87113dde8
adrianarmm <areyemor@myuax.com>
2024-03-06 11:11:08 +0100

    implemento boolean con is + void y sets

--- src/main/java/terrestre.java
+++ src/main/java/terrestre.java
@@ -18,4 +18,39 @@ public class terrestre extends animal{
         this.pelaje = pelaje;
         this.tipo_terrestre = tipo_terrestre;
     }
+
+    public boolean isPatas() {
+        return patas;
+    }
+
+    public void setPatas(boolean patas) {
+        this.patas = patas;
+    }
+
+    public boolean isCola() {
+        return cola;
+    }
+
+    public void setCola(boolean cola) {
+        this.cola = cola;
+    }
+
+    public boolean isPelaje() {
+        return pelaje;
+    }
+
+    public void setPelaje(boolean pelaje) {
+        this.pelaje = pelaje;
+    }
+
+    public String getTipo_terrestre() {
+        return tipo_terrestre;
+    }
+
+    public void setTipo_terrestre(String tipo_terrestre) {
+        this.tipo_terrestre = tipo_terrestre;
+    }
+
+
+
 }
