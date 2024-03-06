962f1d7e52bf95c524a0b0de08ac27f4a39c899b
adrianarmm <areyemor@myuax.com>
2024-03-06 10:38:01 +0100

    uso de strings y boolean

--- src/main/java/ANIMALES/animal.java
+++ src/main/java/ANIMALES/animal.java
@@ -1,4 +1,23 @@
 package ANIMALES;
 
 public class animal {
+
+    String nombre;
+    String especie;
+    String raza;
+    String color;
+    String sexo;
+    String edad;
+    String peso;
+    String altura;
+    String tipo;
+    String habitat;
+    String alimentacion;
+    String reproduccion;
+    String cuidados;
+    String enfermedades;
+    String vacunas;
+    boolean salud;
+    boolean comportamiento;
+    int id;
 }
