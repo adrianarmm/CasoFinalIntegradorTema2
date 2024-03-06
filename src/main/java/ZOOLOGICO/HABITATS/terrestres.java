4f4ceeffd9700fb6d2bbf5d170fe7584d667d3b2
adrianarmm <areyemor@myuax.com>
2024-03-06 11:37:13 +0100

    creacion terrestres

---
+++ src/main/java/ZOOLOGICO/HABITATS/terrestres.java
@@ -0,0 +1,44 @@
+package ZOOLOGICO.HABITATS;
+
+public class terrestres extends habitats{
+    boolean puede_caminar;
+
+    public terrestres(float temperatura,float humedad,boolean limpieza, boolean puede_caminar) {
+        super(temperatura, humedad, limpieza);
+        this.puede_caminar = puede_caminar;
+    }
+
+    @Override
+    public String toString() {
+        return "terrestres []";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+
+    public boolean isPuede_caminar() {
+        return puede_caminar;
+    }
+
+    public void setPuede_caminar(boolean puede_caminar) {
+        this.puede_caminar = puede_caminar;
+    }
+
+}
+}
