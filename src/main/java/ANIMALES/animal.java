6058e2274cf5e839022138e3ab41228c408fe2c4
adrianarmm <areyemor@myuax.com>
2024-03-06 10:38:30 +0100

    creo public animal

--- src/main/java/ANIMALES/animal.java
+++ src/main/java/ANIMALES/animal.java
@@ -20,4 +20,27 @@ public class animal {
     boolean salud;
     boolean comportamiento;
     int id;
+
+    public animal(String nombre, String especie, String raza, String color, String sexo, String edad, String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion, String cuidados, String enfermedades, String vacunas, int id, boolean salud, boolean comportamiento) {
+        this.nombre = nombre;
+        this.especie = especie;
+        this.raza = raza;
+        this.color = color;
+        this.sexo = sexo;
+        this.edad = edad;
+        this.peso = peso;
+        this.altura = altura;
+        this.tipo = tipo;
+        this.habitat = habitat;
+        this.alimentacion = alimentacion;
+        this.reproduccion = reproduccion;
+        this.cuidados = cuidados;
+        this.enfermedades = enfermedades;
+        this.vacunas = vacunas;
+        this.id = id;
+        this.salud = salud;
+        this.comportamiento = comportamiento;
+    }
+
 }
+
