f4393ee49378f60c0042eea98f5296d8990c48de
adrianarmm <areyemor@myuax.com>
2024-03-06 10:58:19 +0100

    creacion public acuatico

--- src/main/java/ANIMALES/acuatico.java
+++ src/main/java/ANIMALES/acuatico.java
@@ -7,4 +7,12 @@ public class acuatico extends animal {ç
     boolean escamas;
     String tipodeagua;
 
+    public acuatico(String nombre, String especie, String raza, String color, String sexo, String edad, String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion, String cuidados, String enfermedades, String vacunas, int id, boolean salud, boolean comportamiento, boolean aletas, boolean branquias, boolean escamas, String tipodeagua) {
+        super(nombre, especie, raza, color, sexo, edad, peso, altura, tipo, habitat, alimentacion, reproduccion, cuidados, enfermedades, vacunas, id, salud, comportamiento);
+        this.aletas = aletas;
+        this.branquias = branquias;
+        this.escamas = escamas;
+        this.tipodeagua = tipodeagua;
+
+
 }
