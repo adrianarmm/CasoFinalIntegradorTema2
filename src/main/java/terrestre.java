86b20555c5315756161192ca76a94cfd58ac197d
adrianarmm <areyemor@myuax.com>
2024-03-06 11:10:34 +0100

    creacion public terrestre

--- src/main/java/terrestre.java
+++ src/main/java/terrestre.java
@@ -6,4 +6,16 @@ public class terrestre extends animal{
     boolean cola;
     boolean pelaje;
     String tipo_terrestre;
+
+    public terrestre(String nombre, String especie, String raza, String color, String sexo, String edad,
+                            String peso, String altura, String tipo, String habitat, String alimentacion, String reproduccion,
+                            String cuidados, String enfermedades, String vacunas, int id,boolean salud, boolean comportamiento,  boolean patas, boolean cola,
+                            boolean pelaje, String tipo_terrestre) {
+        super(nombre, especie, raza, color, sexo, edad, peso, altura, tipo, habitat, alimentacion, reproduccion, cuidados,
+                enfermedades, vacunas, id, salud, comportamiento);
+        this.patas = patas;
+        this.cola = cola;
+        this.pelaje = pelaje;
+        this.tipo_terrestre = tipo_terrestre;
+    }
 }
