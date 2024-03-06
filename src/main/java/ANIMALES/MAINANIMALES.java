2834fe7ef9a7d789d1087eeca4b0734c8c54bc4b
adrianarmm <areyemor@myuax.com>
2024-03-06 11:14:49 +0100

    Mostrar información del delfín

--- src/main/java/ANIMALES/MAINANIMALES.java
+++ src/main/java/ANIMALES/MAINANIMALES.java
@@ -8,4 +8,14 @@ public class MAINANIMALES {
                 "200 kg", "2 m", "Acuático", "Océano", "Peces y calamares", "Vivíparo", "Nadar en aguas profundas",
                 "Fiebre", "Vacuna contra la fiebre", 1, true, true, true, true, true, "Salada");
 
+
+        // Mostrar información del delfín
+        System.out.println("Información del delfín:");
+        System.out.println("Nombre: " + delfin.getNombre());
+        System.out.println("Especie: " + delfin.getEspecie());
+        System.out.println("Color: " + delfin.getColor());
+        System.out.println("Tipo de agua: " + delfin.getTipo_agua());
+        delfin.buenasalud(true);
+        delfin.registrarAlimentacion("Peces");
+
     }
