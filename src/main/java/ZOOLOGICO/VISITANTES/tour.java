43ba14357613ec10d6ecbebfd5399804b676b947
adrianarmm <areyemor@myuax.com>
2024-03-11 19:11:14 +0100

    personalizar tours

--- src/main/java/ZOOLOGICO/VISITANTES/tour.java
+++ src/main/java/ZOOLOGICO/VISITANTES/tour.java
@@ -1,4 +1,12 @@
 package ZOOLOGICO.VISITANTES;
 
 public class tour {
+    protected String descripcion;
+
+    public tour(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public abstract void mostrarTour();
+}
 }
