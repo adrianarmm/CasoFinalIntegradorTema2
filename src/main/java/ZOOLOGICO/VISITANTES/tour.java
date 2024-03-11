798f1b7909ee4db02aa38fa6d77172a7c6b1cc45
adrianarmm <areyemor@myuax.com>
2024-03-11 19:27:29 +0100

    cambios

--- src/main/java/ZOOLOGICO/VISITANTES/tour.java
+++ src/main/java/ZOOLOGICO/VISITANTES/tour.java
@@ -1,35 +1,54 @@
 package ZOOLOGICO.VISITANTES;
 
-public abstract class tour {
-    protected String descripcion;
-
-    public tour(String descripcion) {
-        this.descripcion = descripcion;
+public class TourNinos extends tour {
+    public TourNinos() {
+        super("Tour especial para niños");
     }
 
-    public abstract void mostrarTour();
+    @Override
+    public void mostrarTour() {
+        System.out.println(descripcion + ": incluye juegos y actividades interactivas diseñadas para educar y entretener a los niños.");
+    }
 }
 
-public class TourAves extends tour {
+public class TourNinos extends tour {
+    public TourNinos() {
+        super("Tour especial para niños");
+    }
+
+    @Override
+    public void mostrarTour() {
+        System.out.println(descripcion + ": incluye juegos y actividades interactivas diseñadas para educar y entretener a los niños.");
+    }
+}
+
+public class TourAves extends Tour {
     public TourAves() {
         super("Tour para aficionados a las aves");
     }
 
     @Override
     public void mostrarTour() {
-        System.out.println(descripcion + ": Observación de aves y explicaciones sobre diferentes especies. Incluye recomendaciones para la mejor experiencia de observación.");
+        System.out.println(descripcion + ": ofrece observación de aves, información sobre diferentes especies y consejos para los aficionados.");
     }
 }
 
-public class TourMamiferos extends tour {
+
+public class TourMamiferos extends Tour {
     public TourMamiferos() {
         super("Tour para amantes de los mamíferos");
     }
 
     @Override
     public void mostrarTour() {
-        System.out.println(descripcion + ": Visita a los hábitats de mamíferos y charlas sobre su cuidado y comportamiento. Aprende sobre las especies en peligro de extinción y cómo contribuir a su conservación.");
+        System.out.println(descripcion + ": incluye visitas a los hábitats de los mamíferos y charlas sobre su comportamiento y conservación.");
     }
 }
+
+
+
+
+
+
 
 
