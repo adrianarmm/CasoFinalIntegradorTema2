19e9f948117621ca5051907903b7bf2af2b57965
adrianarmm <areyemor@myuax.com>
2024-03-11 19:18:01 +0100

    cambios

--- src/main/java/ZOOLOGICO/VISITANTES/tour.java
+++ src/main/java/ZOOLOGICO/VISITANTES/tour.java
@@ -10,3 +10,26 @@ public abstract class tour {
     public abstract void mostrarTour();
 }
 
+public class TourAves extends tour {
+    public TourAves() {
+        super("Tour para aficionados a las aves");
+    }
+
+    @Override
+    public void mostrarTour() {
+        System.out.println(descripcion + ": Observación de aves y explicaciones sobre diferentes especies. Incluye recomendaciones para la mejor experiencia de observación.");
+    }
+}
+
+public class TourMamiferos extends tour {
+    public TourMamiferos() {
+        super("Tour para amantes de los mamíferos");
+    }
+
+    @Override
+    public void mostrarTour() {
+        System.out.println(descripcion + ": Visita a los hábitats de mamíferos y charlas sobre su cuidado y comportamiento. Aprende sobre las especies en peligro de extinción y cómo contribuir a su conservación.");
+    }
+}
+
+
