305bf4dee48cf1287970f49f6bf061a98c9cf849
adrianarmm <areyemor@myuax.com>
2024-03-11 19:38:22 +0100

    cambios

---
+++ src/main/java/ZOOLOGICO/RECURSOS/Pedido.java
@@ -0,0 +1,28 @@
+package ZOOLOGICO.RECURSOS;
+
+    import java.util.ArrayList;
+import java.util.List;
+
+    public class Pedido {
+        private List<Recurso> items;
+
+        public Pedido() {
+            this.items = new ArrayList<>();
+        }
+
+        public void agregarItem(Recurso item) {
+            this.items.add(item);
+        }
+
+        public List<Recurso> getItems() {
+            return items;
+        }
+
+        public void mostrarPedido() {
+            for (Recurso item : items) {
+                System.out.println(item);
+            }
+        }
+    }
+
+
