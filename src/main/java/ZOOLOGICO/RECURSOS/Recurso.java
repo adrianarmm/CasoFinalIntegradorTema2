fc0bfa88a427442ac7f347c8eeed2dc4c1d93b07
adrianarmm <areyemor@myuax.com>
2024-03-11 19:35:51 +0100

    nuevo package

---
+++ src/main/java/ZOOLOGICO/RECURSOS/Recurso.java
@@ -0,0 +1,37 @@
+package ZOOLOGICO.RECURSOS;
+
+public class Recurso {public class Recurso {
+    private String nombre;
+    private int cantidad;
+
+    public Recurso(String nombre, int cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    // Getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    @Override
+    public String
+    toString() {
+        return "Recurso{" +
+                "nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                '}';
+    }
+
