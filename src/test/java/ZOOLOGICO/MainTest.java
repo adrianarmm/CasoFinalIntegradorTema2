17593aa3c0a28971caf72d108b99dc1f2374edf5
adrianarmm <areyemor@myuax.com>
2024-03-12 21:35:41 +0100

    cambios

---
+++ src/test/java/ZOOLOGICO/MainTest.java
@@ -0,0 +1,56 @@
+package ZOOLOGICO;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+@Test
+    void testMain() {
+        String[] args = null;
+        Main.main(args);
+        assertTrue(true);
+    }
+
+    @Test
+    void testMain2() {
+        String[] args = null;
+        Main.main(args);
+        assertTrue(true);
+    }
+
+    @Test
+    void testMain3() {
+        String[] args = null;
+        Main.main(args);
+        assertTrue(true);
+    }
+
+    @Test
+    void testMain4() {
+        String[] args = null;
+        Main.main(args);
+        assertTrue(true);
+    }
+
+    @Test
+    void testMain5() {
+        String[] args = null;
+        Main.main(args);
+        assertTrue(true);
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+    }
+}
